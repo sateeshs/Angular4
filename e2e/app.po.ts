091581d0b9e664dfb117d5f39f19301098e04359
ssateesh <sateesh.sabbineni@hotmail.com>
2017-08-19 22:36:41 -0400

    city search tests added

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,39 @@
+import { defineSupportCode, } from 'cucumber';
 import { browser, by, element } from 'protractor';
-
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo() {
+        return browser.get('/');
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getParagraphText() {
+        return element(by.css('app-root h2')).getText();
+    }
+
+    getCitySearch(text: string) {
+        browser.waitForAngularEnabled();
+        var input = element(by.css('md-input-container input#citySearchText'));
+        //element(by.id('citySearchText'));
+
+        //input = element(by.css('md-input-container input#citySearchText'));
+        //input.isElementPresent('.md-input-container').then((val) => { console.log('test Log'+val); });
+        //input = element(by.tagName('.md-card.app-city-search.form.md-input-container.div.div.div.input'));
+
+        input.sendKeys(text);
+
+        browser.sleep(1000);
+
+        element(by.css('.mat-autocomplete-visible')).all(by.tagName('md-option')).each((o, i) => {
+            o.click();
+            browser.waitForAngular(); // wait for the renderings to take effect
+
+        });
+
+
+        //element(by.cssContainingText('.mat-input-hint-wrapper mat-form-field-hint-wrapper', text)).click()
+
+        //expect(input.getAttribute('value')).toBe('repalle, in');
+
+        return element(by.css('app-root h2')).getText();
+
+    }
 }
